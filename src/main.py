73a2a18ecf7194dd8bab71d3ed672f937dd9e111
Mike Borthwick <mhborthwick@gmail.com>
2023-11-05 22:33:00 -0800

    create a little flame

--- src/main.py
+++ src/main.py
@@ -1,19 +1,63 @@
 import typer
+import time
+from rich import print
+from rich.console import Console
 
 app = typer.Typer()
+
+console = Console()
+
+
+def print_campfire():
+    campfire_frames = [
+        [
+            "    , , , ,   ",
+            "     , , ,    ",
+            "      , ,     ",
+            "     , , ,    ",
+            "    , , , ,   ",
+        ],
+        [
+            "     , , ,    ",
+            "      , ,     ",
+            "     , , ,    ",
+            "    , , , ,   ",
+            "   , , , , ,  ",
+        ],
+        [
+            "      , ,     ",
+            "     , , ,    ",
+            "    , , , ,   ",
+            "   , , , , ,  ",
+            "  , , , , , , ",
+        ],
+        [
+            "     , , ,    ",
+            "    , , , ,   ",
+            "   , , , , ,  ",
+            "  , , , , , , ",
+            " , , , , , , ,",
+        ],
+    ]
+    try:
+        while True:
+            for frame in campfire_frames:
+                console.clear()
+                for line in frame:
+                    colored_line = (
+                        "[bold red]" + line[:5] + "[/bold red]" +
+                        "[bold yellow]" + line[5:] + "[/bold yellow]"
+                    )
+                    print(colored_line)
+                print()
+                time.sleep(0.2)
+    except KeyboardInterrupt:
+        console.print("[bold]Campfire animation stopped![/bold]")
 
 
 @app.command()
 def start_campfire():
-    hello = """
-██╗  ██╗███████╗██╗     ██╗      ██████╗ 
-██║  ██║██╔════╝██║     ██║     ██╔═══██╗
-███████║█████╗  ██║     ██║     ██║   ██║
-██╔══██║██╔══╝  ██║     ██║     ██║   ██║
-██║  ██║███████╗███████╗███████╗╚██████╔╝
-╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝
-"""
-    print(hello)
+    print_campfire()
 
 
 if __name__ == "__main__":
