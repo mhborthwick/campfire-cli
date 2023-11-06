75cf5d3120cb569391952bf9c5b3a1455e81a32d
Mike Borthwick <mhborthwick@gmail.com>
2023-11-05 23:53:39 -0800

    refactor

--- src/main.py
+++ src/main.py
@@ -8,7 +8,25 @@ app = typer.Typer()
 console = Console()
 
 
-def print_campfire():
+def print_campfire(frames: list[list[str]]):
+    try:
+        while True:
+            for frame in frames:
+                console.clear()
+                for line in frame:
+                    colored_line = (
+                        "[bold red]" + line[:5] + "[/bold red]" +
+                        "[bold yellow]" + line[5:] + "[/bold yellow]"
+                    )
+                    print(colored_line)
+                print()
+                time.sleep(0.2)
+    except KeyboardInterrupt:
+        console.print("[bold]Campfire animation stopped![/bold]")
+
+
+@app.command()
+def start_campfire():
     campfire_frames = [
         [
             "    , , , ,   ",
@@ -39,25 +57,7 @@ def print_campfire():
             " , , , , , , ,",
         ],
     ]
-    try:
-        while True:
-            for frame in campfire_frames:
-                console.clear()
-                for line in frame:
-                    colored_line = (
-                        "[bold red]" + line[:5] + "[/bold red]" +
-                        "[bold yellow]" + line[5:] + "[/bold yellow]"
-                    )
-                    print(colored_line)
-                print()
-                time.sleep(0.2)
-    except KeyboardInterrupt:
-        console.print("[bold]Campfire animation stopped![/bold]")
-
-
-@app.command()
-def start_campfire():
-    print_campfire()
+    print_campfire(campfire_frames)
 
 
 if __name__ == "__main__":
