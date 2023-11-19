27008488772468744d60ccc0144edf62e85c2c2d
Mike Borthwick <mhborthwick@gmail.com>
2023-11-19 13:36:27 -0800

    refactor for testability

--- src/main.py
+++ src/main.py
@@ -2,9 +2,7 @@ import os
 import typer
 import time
 import pygame
-from rich import print
 from rich.console import Console
-
 
 app = typer.Typer()
 
@@ -14,54 +12,66 @@ rocks = "[bold grey50] (( )( )( )( )) [/bold grey50]"
 grass = "[bold medium_spring_green] `````````````` [/bold medium_spring_green]"
 
 
-def initialize_audio(sound_file):
-    pygame.mixer.init()
-    pygame.mixer.music.load(sound_file)
-    pygame.mixer.music.set_volume(0.5)
+class AudioPlayer:
+    def initialize(self, sound_file):
+        pygame.mixer.init()
+        pygame.mixer.music.load(sound_file)
+        pygame.mixer.music.set_volume(0.5)
 
+    def play(self):
+        pygame.mixer.music.play(loops=-1)
 
-def play_audio():
-    pygame.mixer.music.play(loops=-1)
+    def stop(self):
+        pygame.mixer.music.stop()
 
-
-def stop_audio():
-    pygame.mixer.music.stop()
-
-
-def print_campfire(frames: list[list[str]], sound=False, verbose=True):
-    if sound:
+    def get_audio_path(self):
         script_path = os.path.abspath(__file__)
         script_dir = os.path.dirname(script_path)
-        audio_path = os.path.join(script_dir, "audio", "campfire_sound.mp3")
-        initialize_audio(audio_path)
-        play_audio()
-    try:
-        while True:
-            for frame in frames:
-                console.clear()
-                for i, line in enumerate(frame):
-                    sway = " " * (i % 2)
-                    colored_line = (
-                        "[bold red]" + sway + line[:5] + "[/bold red]" +
-                        "[bold yellow]" + line[5:] + "[/bold yellow]"
-                    )
-                    print(colored_line)
-                print(rocks)
-                print(grass)
-                if verbose:
-                    print(f'sound={sound}')
-                print()
-                time.sleep(0.2)
-    except KeyboardInterrupt:
-        if sound:
-            stop_audio()
-        console.print("[bold]Campfire animation stopped![/bold]")
+        return os.path.join(script_dir, "audio", "campfire_sound.mp3")
+
+
+class CampfirePrinter:
+    def __init__(self, console, audio_player):
+        self.console = console
+        self.audio_player = audio_player
+
+    def print_frame(self, frame):
+        for i, line in enumerate(frame):
+            sway = " " * (i % 2)
+            colored_line = (
+                "[bold red]" + sway + line[:5] + "[/bold red]" +
+                "[bold yellow]" + line[5:] + "[/bold yellow]"
+            )
+            self.console.print(colored_line)
+        self.console.print(rocks)
+        self.console.print(grass)
+
+    def print_campfire(self, frames, sound, verbose):
+        try:
+            if sound:
+                audio_path = self.audio_player.get_audio_path()
+                self.audio_player.initialize(audio_path)
+                self.audio_player.play()
+            while True:
+                for frame in frames:
+                    self.console.clear()
+                    self.print_frame(frame)
+                    if verbose:
+                        self.console.print(f'sound={sound}')
+                    self.console.print()
+                    time.sleep(0.2)
+        except KeyboardInterrupt:
+            if sound:
+                self.audio_player.stop()
+            self.console.print("[bold]Campfire animation stopped![/bold]")
 
 
 @app.command()
 def start_campfire(
         sound: bool = typer.Option(False, help="Enable campfire sound"),
         verbose: bool = typer.Option(True, help="Display campfire sound on/off message")):
+    audio_player = AudioPlayer()
+    campfire_printer = CampfirePrinter(console, audio_player)
     campfire_frames = [
         [
             "    , , , `   ",
@@ -92,7 +102,8 @@ def start_campfire(
             " , , , , , , ,",
         ],
     ]
-    print_campfire(campfire_frames, sound=sound, verbose=verbose)
+    campfire_printer.print_campfire(
+        campfire_frames, sound=sound, verbose=verbose)
 
 
 if __name__ == "__main__":
